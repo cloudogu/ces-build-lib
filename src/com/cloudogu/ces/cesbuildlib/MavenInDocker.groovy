14f4dd386dac1dbb33754968c056a883a2112e35
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2017-07-11 18:22:49 +0200

    MavenInDocker: Added option for enabling docker host in maven.
    This allows for starting docker containers from maven.

--- src/com/cloudogu/ces/cesbuildlib/MavenInDocker.groovy
+++ src/com/cloudogu/ces/cesbuildlib/MavenInDocker.groovy
@@ -1,14 +1,20 @@
 package com.cloudogu.ces.cesbuildlib
 /**
  * Run maven in a docker container.
- * This can be helpful, when constants ports are bound during the build and concurrent builds cause port conflicts.
- * For example, when running integration tests.
  *
- * The builds base on the official maven containers from https://hub.docker.com/_/maven/
+ * This can be helpful,
+ * * when constant ports are bound during the build that cause port conflicts in concurrent builds.
+ *   For example, when running integration tests, unit tests that use infrastructure that binds to ports or
+ * * when one maven repo per builds is required
+ *   For example when concurrent builds of multi module project install the same snapshot versions.
+ *
+ * The build are run inside the official maven containers from https://hub.docker.com/_/maven/
  */
 class MavenInDocker extends Maven {
 
     String dockerImageVersion
+    /** Setting this to {@code true} allows the maven build to access the docker host, i.e. to start other containers.*/
+    boolean enableDockerHost = false
 
     /**
      * @param script the Jenkinsfile instance ({@code this} in Jenkinsfile)
@@ -21,39 +27,77 @@ class MavenInDocker extends Maven {
 
     @Override
     def mvn(String args) {
-        String jenkinsHome = script.env.HOME
-        String commandLineArgs = createCommandLineArgs(args)
+        writeSettingsXml()
+        writeDockerFile()
 
-        writeDockerFile(dockerImageVersion)
-
+        // Jenkins docker plugin automatically mounts current workspace as working dir of container
+        // So, set it consistently as env var and system property
         script.withEnv(["HOME=${script.pwd()}"]) {
             script.docker.build("ces-build-lib/maven/$dockerImageVersion", createDockerfilePath())
-                    .inside("--volume=\"${jenkinsHome}/.m2/repository:${script.pwd()}/.m2/repository:rw\"") {
-                script.sh "mvn ${commandLineArgs} -Duser.home=\"${script.pwd()}\""
+                    .inside(createDockerRunArgs()) {
+                script.sh "mvn -Duser.home=\"${script.pwd()}\" -s \"${script.pwd()}/.m2/settings.xml\"  " +
+                        "${createCommandLineArgs(args)}"
             }
         }
+    }
+
+    String createDockerRunArgs() {
+        if (enableDockerHost) {
+            "-v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_HOST=\"unix:///var/run/docker.sock\" --group-add ${readDockerGroupId()}"
+        } else {
+            ""
+        }
+    }
+
+    /**
+     * Create settings.xml in workspace, pointing to .m2 repo ins workspace
+     */
+    void writeSettingsXml() {
+        script.writeFile file: "${script.pwd()}/.m2/settings.xml", text: """
+            <settings>
+                <localRepository>${script.pwd()}/.m2/repository</localRepository>
+            </settings>"""
     }
 
     /**
      * Write dockerfile, basing on desired image version and containing jenkins user
      */
-    String writeDockerFile(String dockerImageVersion) {
-        String dockerfile =
-                "FROM maven:$dockerImageVersion\n" +
-                "RUN echo \"${readJenkinsUserFromEtcPasswd()}\" >> /etc/passwd\n" +
-                'USER JENKINS'
-
+    String writeDockerFile() {
         def dockerfilePath = "${script.pwd()}/" + createDockerfilePath() + "/Dockerfile"
-        script.writeFile file: dockerfilePath, text: "${dockerfile}"
+
+        /* Add jenkins user. This is necessary for some commands such as npm.
+           Add docker group. This is necessary to access Docker host from maven.*/
+        script.writeFile file: dockerfilePath, text:  """
+            FROM maven:$dockerImageVersion
+            RUN echo \\"${readJenkinsUserFromEtcPasswd()}\\" >> /etc/passwd \
+            && echo \\"${readDockerGroupFromEtcGroup()}\\" >> /etc/group"""
         return dockerfilePath
     }
 
+    String readDockerGroupFromEtcGroup() {
+        // Get the GID of the docker group, e.g. "docker:x:999:jenkins"
+        def dockerGroupEtcGroup = script.sh(returnStdout: true,
+                script: 'cat /etc/group | grep docker')
+                .trim()
+        if (dockerGroupEtcGroup == null || dockerGroupEtcGroup.isEmpty()) {
+            script.echo "WARN: Unable to parse group docker from /etc/group. Docker host will not be accessible for maven."
+        }
+        dockerGroupEtcGroup
+    }
+
+    String readDockerGroupId() {
+        // Get the GID of the docker group
+        script.sh(returnStdout: true,
+                script: "echo ${readDockerGroupFromEtcGroup()} | sed -E 's/.*:x:(.*):.*/\\1/'")
+                .trim()
+    }
+
     String readJenkinsUserFromEtcPasswd() {
-        // Query current Docker user string, e.g. "jenkins:x:1000:1000:Jenkins,,,:/home/jenkins:/bin/bash"
+        // Query current jenkins user string, e.g. "jenkins:x:1000:1000:Jenkins,,,:/home/jenkins:/bin/bash"
         // An alternative (dirtier) approach: https://github.com/cloudogu/docker-golang/blob/master/Dockerfile
         String jenkinsUserFromEtcPasswd = script.sh(returnStdout: true,
-                script: 'cat /etc/passwd | grep jenkins'
-        ).trim()
+                script: 'cat /etc/passwd | grep jenkins')
+                .trim()
         if (jenkinsUserFromEtcPasswd == null || jenkinsUserFromEtcPasswd.isEmpty()) {
             script.echo "WARN: Unable to parse user jenkins from /etc/passwd. Maven build will fail."
             // It would be wonderful if we could use exceptions in Jenkins Shared libraries...
--- test/com/cloudogu/ces/cesbuildlib/MavenInDockerTest.groovy
+++ test/com/cloudogu/ces/cesbuildlib/MavenInDockerTest.groovy
@@ -6,24 +6,27 @@ import static junit.framework.TestCase.assertEquals
 import static junit.framework.TestCase.assertTrue
 
 class MavenInDockerTest {
+    public static final String EXPECTED_JENKINS_USER_FROM_ETC_PASSWD =
+            "jenkins:x:1000:1000:Jenkins,,,:/home/jenkins:/bin/bash"
+    public static final String EXPECTED_DOCKER_HOST = "172.17.0.1"
 
+    String expectedVersion = "3.5.0-jdk8"
 
-    public static
-    final String EXPECTED_JENKINS_USER_FROM_ETC_PASSWD = "jenkins:x:1000:1000:Jenkins,,,:/home/jenkins:/bin/bash"
+    def scriptMock = new ScriptMock()
+
+    def mavenInDocker = new MavenInDocker(scriptMock, expectedVersion)
 
     @Test
     void testWriteDockerFile() {
-        def scriptMock = new ScriptMock()
-        String expectedVersion = "3.5.0-jdk8"
 
-        new MavenInDocker(scriptMock, expectedVersion).writeDockerFile(expectedVersion)
+        mavenInDocker.writeDockerFile()
 
         assertTrue("script.writeFile() not called", scriptMock.getWriteFileParams().size() > 0)
         String actualDockerfile =  scriptMock.writeFileParams.get(0).get("text")
-        assertTrue("Expected version $expectedVersion not contained in actual file: $actualDockerfile",
+        assertTrue("Expected version $expectedVersion not contained in actual dockerfile: $actualDockerfile",
                 actualDockerfile.contains(expectedVersion))
-        assertTrue("Expected version $expectedVersion not contained in actual file: $actualDockerfile",
-                actualDockerfile.contains("RUN echo \"$EXPECTED_JENKINS_USER_FROM_ETC_PASSWD\" >> /etc/passwd"))
+        assertTrue("Expected user $EXPECTED_JENKINS_USER_FROM_ETC_PASSWD not contained in actual dockerfile: $actualDockerfile",
+                actualDockerfile.contains(EXPECTED_JENKINS_USER_FROM_ETC_PASSWD))
         String actualDockerfilePath =  scriptMock.writeFileParams.get(0).get("file")
         assertEquals("/.jenkins/build/$expectedVersion/Dockerfile", actualDockerfilePath)
     }
