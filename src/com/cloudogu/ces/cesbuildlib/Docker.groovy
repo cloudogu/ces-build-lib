12957c558b1c71f3d5d6ca15dd2db50622e01021
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2017-09-04 08:54:49 +0200

    Adds javadoc to the docker steps from Jenkins.

--- src/com/cloudogu/ces/cesbuildlib/Docker.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Docker.groovy
@@ -26,25 +26,62 @@ class Docker implements Serializable {
        https://github.com/jenkinsci/docker-workflow-plugin/blob/master/src/main/resources/org/jenkinsci/plugins/docker/workflow/Docker.groovy
      */
 
-    // TODO copy doc from Jenkins and add some examples for calling
-
-
+    /**
+     * Specifies a registry URL such as https://docker.mycorp.com/, plus an optional credentials ID to connect to it.
+     *
+     * Example:
+     * <pre>
+     *   def dockerImage = docker.build("image/name:1.0", "folderOfDockfile")
+     *   docker.withRegistry("https://your.registry", 'credentialsId') {
+     *       dockerImage.push()
+     *   }
+     *  </pre>
+     */
     def withRegistry(String url, String credentialsId = null, Closure body) {
         return this.script.docker.withRegistry( url, credentialsId, body)
     }
 
+    /**
+     * Specifies a server URI such as tcp://swarm.mycorp.com:2376, plus an optional credentials ID to connect to it.
+     */
     def withServer(String uri, String credentialsId = null, Closure body) {
         return this.script.docker.withServer(uri, credentialsId, body)
     }
 
+    /**
+     * Specifies the name of a Docker installation to use, if any are defined in Jenkins global configuration.
+     * If unspecified, docker is assumed to be in the $PATH of the Jenkins agent.
+     */
     def withTool(String toolName, Closure body) {
         return this.script.docker.withTool(toolName, body)
     }
 
+    /**
+     * Creates an Image object with a specified name or ID.
+     *
+     * Example:
+     * <pre>
+     *      docker.image('google/cloud-sdk:164.0.0').inside("-e HOME=${pwd()}") {
+     *          sh "echo something"
+     *      }
+     *  </pre>
+     */
     def image(String id) {
         return this.script.docker.image(id)
     }
 
+    /**
+     * Runs docker build to create and tag the specified image from a Dockerfile in the current directory.
+     * Additional args may be added, such as '-f Dockerfile.other --pull --build-arg http_proxy=http://192.168.1.1:3128 .'.
+     * Like docker build, args must end with the build context.
+     *
+     * Example:
+     * <pre>
+     *   def dockerContainer = docker.build("image/name:1.0", "folderOfDockfile").run("-e HOME=${pwd()}")
+     *  </pre>
+     *
+     * @return the resulting Image object. Records a FROM fingerprint in the build
+     */
     def build(String image, String args = '.') {
         return this.script.docker.build(image, args)
     }
@@ -53,7 +90,6 @@ class Docker implements Serializable {
      * @param container docker container instance
      * @return the IP address for a docker container instance
      */
-    // Note this method must not be private, in order to avoid RejectedAccessException: unclassified method java
     String hostIp(container) {
         String ip =
                 script.sh(returnStdout: true,
