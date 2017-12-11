b3e8ec5e31d8602ad339e9afb83df997a29613eb
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2017-12-11 13:53:52 +0100

    Implements all current docker methods within the Docker.Image class.
    Basis for extending image. Why re-implement? Same as Docker class: The spread operator (*) is still not supported for Jenkins pipelines.

--- src/com/cloudogu/ces/cesbuildlib/Docker.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Docker.groovy
@@ -15,80 +15,6 @@ class Docker implements Serializable {
         this.sh = new Sh(script)
     }
 
-    /* Define methods of global docker variable again.
-       Using methodMissing(String name, args) is not possible, because we would need the spread operator for dynamic
-       arguments (*) which is not supported by Jenkins Pipeline
-
-        def methodMissing(String name, args) {
-            return this.script.docker."$name"(*args)
-        }
-
-       Leads to UnsupportedOperationException.
-
-       Instead ust delegate all methods manually. See
-       https://github.com/jenkinsci/docker-workflow-plugin/blob/master/src/main/resources/org/jenkinsci/plugins/docker/workflow/Docker.groovy
-     */
-
-    /**
-     * Specifies a registry URL such as https://docker.mycorp.com/, plus an optional credentials ID to connect to it.
-     *
-     * Example:
-     * <pre>
-     *   def dockerImage = docker.build("image/name:1.0", "folderOfDockfile")
-     *   docker.withRegistry("https://your.registry", 'credentialsId') {
-     *       dockerImage.push()
-     *   }
-     *  </pre>
-     */
-    def withRegistry(String url, String credentialsId = null, Closure body) {
-        return this.script.docker.withRegistry( url, credentialsId, body)
-    }
-
-    /**
-     * Specifies a server URI such as tcp://swarm.mycorp.com:2376, plus an optional credentials ID to connect to it.
-     */
-    def withServer(String uri, String credentialsId = null, Closure body) {
-        return this.script.docker.withServer(uri, credentialsId, body)
-    }
-
-    /**
-     * Specifies the name of a Docker installation to use, if any are defined in Jenkins global configuration.
-     * If unspecified, docker is assumed to be in the $PATH of the Jenkins agent.
-     */
-    def withTool(String toolName, Closure body) {
-        return this.script.docker.withTool(toolName, body)
-    }
-
-    /**
-     * Creates an Image object with a specified name or ID.
-     *
-     * Example:
-     * <pre>
-     *      docker.image('google/cloud-sdk:164.0.0').inside("-e HOME=${pwd()}") {
-     *          sh "echo something"
-     *      }
-     *  </pre>
-     */
-    def image(String id) {
-        return this.script.docker.image(id)
-    }
-
-    /**
-     * Runs docker build to create and tag the specified image from a Dockerfile in the current directory.
-     * Additional args may be added, such as '-f Dockerfile.other --pull --build-arg http_proxy=http://192.168.1.1:3128 .'.
-     * Like docker build, args must end with the build context.
-     *
-     * Example:
-     * <pre>
-     *   def dockerContainer = docker.build("image/name:1.0", "folderOfDockfile").run("-e HOME=${pwd()}")
-     *  </pre>
-     *
-     * @return the resulting Image object. Records a FROM fingerprint in the build
-     */
-    def build(String image, String args = '.') {
-        return this.script.docker.build(image, args)
-    }
-
     /**
      * @param container docker container instance
      * @return the IP address for a docker container instance
@@ -111,5 +37,141 @@ class Docker implements Serializable {
      */
     boolean isRunning(container) {
         return Boolean.valueOf(sh.returnStdOut("docker inspect -f {{.State.Running}} ${container.id}"))
+    }
+
+    /* Define methods of global docker variable again.
+       Using methodMissing(String name, args) is not possible, because we would need the spread operator for dynamic
+       arguments (*) which is not supported by Jenkins Pipeline
+
+        def methodMissing(String name, args) {
+            return this.script.docker."$name"(*args)
+        }
+
+       Leads to UnsupportedOperationException.
+
+       Instead ust delegate all methods manually. See
+       https://github.com/jenkinsci/docker-workflow-plugin/blob/master/src/main/resources/org/jenkinsci/plugins/docker/workflow/Docker.groovy
+     */
+
+    /**
+     * Specifies a registry URL such as https://docker.mycorp.com/, plus an optional credentials ID to connect to it.
+     *
+     * Example:
+     * <pre>
+     *   def dockerImage = docker.build("image/name:1.0", "folderOfDockfile")
+     *   docker.withRegistry("https://your.registry", 'credentialsId') {*       dockerImage.push()
+     *}*  </pre>
+     */
+    def withRegistry(String url, String credentialsId = null, Closure body) {
+        return this.script.docker.withRegistry(url, credentialsId, body)
+    }
+
+    /**
+     * Specifies a server URI such as tcp://swarm.mycorp.com:2376, plus an optional credentials ID to connect to it.
+     */
+    def withServer(String uri, String credentialsId = null, Closure body) {
+        return this.script.docker.withServer(uri, credentialsId, body)
+    }
+
+    /**
+     * Specifies the name of a Docker installation to use, if any are defined in Jenkins global configuration.
+     * If unspecified, docker is assumed to be in the $PATH of the Jenkins agent.
+     */
+    def withTool(String toolName, Closure body) {
+        return this.script.docker.withTool(toolName, body)
+    }
+
+    /**
+     * Creates an Image object with a specified name or ID.
+     *
+     * Example:
+     * <pre>
+     *      docker.image('google/cloud-sdk:164.0.0').inside("-e HOME=${pwd()}") {*          sh "echo something"
+     *}*  </pre>
+     */
+    def image(String id) {
+        return new Image(this.script, id)
+    }
+
+    /**
+     * Runs docker build to create and tag the specified image from a Dockerfile in the current directory.
+     * Additional args may be added, such as '-f Dockerfile.other --pull --build-arg http_proxy=http://192.168.1.1:3128 .'.
+     * Like docker build, args must end with the build context.
+     *
+     * Example:
+     * <pre>
+     *   def dockerContainer = docker.build("image/name:1.0", "folderOfDockfile").run("-e HOME=${pwd()}")
+     *  </pre>
+     *
+     * @return the resulting Image object. Records a FROM fingerprint in the build
+     */
+    def build(String image, String args = '.') {
+        return this.script.docker.build(image, args)
+    }
+
+    static class Image implements Serializable {
+
+        private final script
+        private image
+        /** The image name with optional tag (mycorp/myapp, mycorp/myapp:latest) or ID (hexadecimal hash).  **/
+        String id
+
+        private Image(script, String id) {
+            this.script = script
+            image = script.docker.image(id)
+            this.id = image.id
+        }
+
+        String imageName() {
+            return image.imageName()
+        }
+
+        /**
+         * Like withRun this starts a container for the duration of the body, but all external commands (sh) launched
+         * by the body run inside the container rather than on the host. These commands run in the same working
+         * directory (normally a Jenkins agent workspace), which means that the Docker server must be on localhost.
+         */
+        def inside(String args = '', Closure body) {
+            return image.inside(args, body)
+        }
+
+        /**
+         * Runs docker pull. Not necessary before run, withRun, or inside.
+         */
+        void pull() {
+            script.docker.pull()
+        }
+
+        /**
+         *  Uses docker run to run the image, and returns a Container which you could stop later. Additional args may
+         *  be added, such as '-p 8080:8080 --memory-swap=-1'. Optional command is equivalent to Docker command
+         *  specified after the image. Records a run fingerprint in the build.
+         */
+        def run(String args = '', String command = "") {
+            return image.run(args, command)
+        }
+
+        /**
+         * Like run but stops the container as soon as its body exits, so you do not need a try-finally block.
+         */
+        def withRun(String args = '', String command = "", Closure body) {
+            return image.withRun(args, command, body)
+        }
+
+        /**
+         * Runs docker tag to record a tag of this image (defaulting to the tag it already has). Will rewrite an
+         * existing tag if one exists.
+         */
+        void tag(String tagName = image.parsedId.tag, boolean force = true) {
+            image.tag(tagName, force)
+        }
+
+        /**
+         * Pushes an image to the registry after tagging it as with the tag method. For example, you can use image.push
+         * 'latest' to publish it as the latest version in its repository.
+         */
+        void push(String tagName = image.parsedId.tag, boolean force = true) {
+            image.push(tagName, force)
+        }
     }
 }
--- test/com/cloudogu/ces/cesbuildlib/DockerTest.groovy
+++ test/com/cloudogu/ces/cesbuildlib/DockerTest.groovy
@@ -2,16 +2,37 @@ package com.cloudogu.ces.cesbuildlib
 
 import org.junit.Test
 
-import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertNull
-import static org.junit.Assert.assertTrue
+import static org.junit.Assert.*
 
 class DockerTest {
 
     @Test
+    void findIp() {
+        String containerId = '93a401b14684'
+        Docker docker = new Docker([sh: { Map<String, String> args -> return args['script'] }])
+        def ip = docker.findIp([id: containerId])
+        assertTrue(ip.contains(containerId))
+    }
+
+    @Test
+    void findEnv() {
+        String containerId = '93a401b14684'
+        Docker docker = new Docker([sh: { Map<String, String> args -> return args['script'] }])
+        def evn = docker.findEnv([id: containerId])
+        assertTrue(evn.contains(containerId))
+    }
+
+    @Test
+    void isRunning() {
+        String containerId = '93a401b14684'
+        Docker docker = new Docker([sh: { Map<String, String> args -> return "true" }])
+        assertTrue(docker.isRunning([id: containerId]))
+    }
+
+    @Test
     void withRegistry() {
 
-        Docker docker = create([ withRegistry: { String param1, String param2, Closure param3 ->
+        Docker docker = create([withRegistry: { String param1, String param2, Closure param3 ->
             return [param1, param2, param3]
         }])
 
@@ -25,7 +46,7 @@ class DockerTest {
 
     @Test
     void withRegistryWithoutCredentials() {
-        Docker docker = create([ withRegistry: { String param1, String param2, Closure param3 ->
+        Docker docker = create([withRegistry: { String param1, String param2, Closure param3 ->
             return [param1, param2, param3]
         }])
 
@@ -41,7 +62,7 @@ class DockerTest {
     @Test
     void withServer() {
 
-        Docker docker = create([ withServer: { String param1, String param2, Closure param3 ->
+        Docker docker = create([withServer: { String param1, String param2, Closure param3 ->
             return [param1, param2, param3]
         }])
 
@@ -55,7 +76,7 @@ class DockerTest {
 
     @Test
     void withServerWithoutCredentials() {
-        Docker docker = create([ withServer: { String param1, String param2, Closure param3 ->
+        Docker docker = create([withServer: { String param1, String param2, Closure param3 ->
             return [param1, param2, param3]
         }])
 
@@ -70,7 +91,7 @@ class DockerTest {
 
     @Test
     void withTool() {
-        Docker docker = create([ withTool: { String param1, Closure param2 ->
+        Docker docker = create([withTool: { String param1, Closure param2 ->
             return [param1, param2]
         }])
 
@@ -83,19 +104,8 @@ class DockerTest {
     }
 
     @Test
-    void image() {
-        Docker docker = create([ image: { String param1 ->
-            return param1
-        }])
-
-        def ret = docker.image('google/cloud-sdk:164.0.0')
-
-        assertEquals('google/cloud-sdk:164.0.0', ret)
-    }
-
-    @Test
     void build() {
-        Docker docker = create([ build: { String param1, String param2 ->
+        Docker docker = create([build: { String param1, String param2 ->
             return [param1, param2]
         }])
 
@@ -107,7 +117,7 @@ class DockerTest {
 
     @Test
     void buildWithoutArgs() {
-        Docker docker = create([ build: { String param1, String param2 ->
+        Docker docker = create([build: { String param1, String param2 ->
             return [param1, param2]
         }])
 
@@ -118,31 +128,89 @@ class DockerTest {
     }
 
     @Test
-    void findIp() {
-        String containerId = '93a401b14684'
-        Docker docker = new Docker( [ sh: { Map<String, String> args -> return args['script'] } ])
-        def ip = docker.findIp([id: containerId])
-        assertTrue(ip.contains(containerId))
+    void imageInside() {
+        def expectedImage = 'google/cloud-sdk:164.0.0'
+
+        Docker docker = createWithImage(expectedImage,
+                [inside: { String param1, Closure param2 ->
+                    return [param1, param2]
+                }])
+
+        def args = docker.image(expectedImage).inside('-v a:b') { return 'expectedClosure' }
+
+        assertEquals('-v a:b', args[0])
+        assertEquals('expectedClosure', args[1].call())
     }
 
     @Test
-    void findEnv() {
-        String containerId = '93a401b14684'
-        Docker docker = new Docker( [ sh: { Map<String, String> args -> return args['script'] } ])
-        def evn = docker.findEnv([id: containerId])
-        assertTrue(evn.contains(containerId))
+    void imageId() {
+        def expectedImage = 'google/cloud-sdk:164.0.0'
+
+        Docker docker = createWithImage(expectedImage, [imageName: { expectedImage }])
+
+        def args = docker.image(expectedImage).id
+
+        assertEquals(expectedImage, args)
     }
 
     @Test
-    void isRunning() {
-        String containerId = '93a401b14684'
-        Docker docker = new Docker( [ sh: { Map<String, String> args -> return "true" } ])
-        assertTrue(docker.isRunning([id: containerId]))
+    void imageName() {
+        def expectedImage = 'google/cloud-sdk:164.0.0'
+
+        Docker docker = createWithImage(expectedImage, [imageName: { expectedImage }])
+
+        def args = docker.image(expectedImage).imageName()
+
+        assertEquals(expectedImage, args)
+    }
+
+    @Test
+    void imageRun() {
+        def expectedImage = 'google/cloud-sdk:164.0.0'
+
+        Docker docker = createWithImage(expectedImage,
+                [run: { String param1, String param2 ->
+                    return [param1, param2]
+                }])
+
+        def args = docker.image(expectedImage).run('arg', 'cmd')
+
+        assertEquals('arg', args[0])
+        assertEquals('cmd', args[1])
+    }
+
+    @Test
+    void imageWithRun() {
+        def expectedImage = 'google/cloud-sdk:164.0.0'
+
+        Docker docker = createWithImage(expectedImage,
+                [withRun: { String param1, String param2, Closure param3 ->
+                    return [param1, param2, param3]
+                }])
+
+        def args = docker.image(expectedImage).withRun('arg', 'cmd')  { return 'expectedClosure' }
+
+        assertEquals('arg', args[0])
+        assertEquals('cmd', args[1])
+        assertEquals('expectedClosure', args[2].call())
     }
 
     private Docker create(Map<String, Closure> mockedMethod) {
         Map<String, Map<String, Closure>> mockedScript = [
                 docker: mockedMethod
+        ]
+        return new Docker(mockedScript)
+    }
+
+    private Docker createWithImage(String expectedImage, Map<String, Closure> mockedMethod) {
+
+        def mockedScript = [
+                docker: [image: { String id ->
+                    assert id == expectedImage
+                    mockedMethod.put('id', id)
+                    return mockedMethod
+                }
+                ]
         ]
         return new Docker(mockedScript)
     }
