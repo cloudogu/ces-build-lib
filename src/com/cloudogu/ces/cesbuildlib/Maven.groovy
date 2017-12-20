128c77e670710485f8513d3831e9f1e7e4485844
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2017-12-20 17:07:46 +0100

    Maven.deployToNexusRepository(): Adapt to limitations of sandbox

--- src/com/cloudogu/ces/cesbuildlib/Maven.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Maven.groovy
@@ -6,6 +6,10 @@ abstract class Maven implements Serializable {
     // Args added to each mvn call
     String additionalArgs = ""
 
+    // Private vars lead to exceptions when accessing them from methods of this class. So, don't make them private...
+    Repository deploymentRepository = null
+    SignatureCredentials signatureCredentials = null
+
     Maven(script) {
         this.script = script
     }
@@ -39,6 +43,14 @@ abstract class Maven implements Serializable {
         matcher ? matcher[0][1] : ""
     }
 
+    void setDepoymentRepo(String id, String url, String credentialsIdUsernameAndPassword) {
+        deploymentRepository = new Repository(id, url, credentialsIdUsernameAndPassword)
+    }
+
+    void setSignatureCredentials(String publicKeyRingFile, String secretKeyRingFile, String secretKeyPassPhrase) {
+        signatureCredentials = new SignatureCredentials(publicKeyRingFile, secretKeyRingFile, secretKeyPassPhrase)
+    }
+
     /**
      * Signs jar and deploys using the nexus-staging-maven-plugin.
      *
@@ -47,45 +59,66 @@ abstract class Maven implements Serializable {
      *
      * 'ossrh' // Sonatype OSSRH (OSS Repository Hosting)
      */
-    void signAndDeployToNexusRepository(SignatureCredentials signatureCredentials, Repository repository, String additionalDeployArgs = '') {
-        script.withCredentials([script.file(credentialsId: signatureCredentials.publicKeyRingFile, variable: 'pubring'),
-                                script.file(credentialsId: signatureCredentials.secretKeyRingFile, variable: 'secring'),
-                                script.string(credentialsId: signatureCredentials.secretKeyPassPhrase, variable: 'passphrase')
-        ]) {
-            additionalDeployArgs =
-                    // Sign jar using gpg
-                    "gpg:sign -Dgpg.publicKeyring=$publicKeyring -Dgpg.secretKeyring=$privateKeyring -Dgpg.passphrase=$passphrase " +
-                    // Use nexus-staging-maven-plugin instead of maven-deploy-plugin
-                    // https://github.com/sonatype/nexus-maven-plugins/tree/master/staging/maven-plugin#maven2-only-or-explicit-maven3-mode
-                    "org.sonatype.plugins:nexus-staging-maven-plugin:deploy -Dmaven.deploy.skip=true " +
-                    "-DserverId=${repository.id} -DnexusUrl=${repository.url} -DautoReleaseAfterClose=true " +
-                    additionalDeployArgs
 
-            deployToNexusRepository(repository, additionalDeployArgs)
+    void deployToNexusRepository(String additionalDeployArgs = '') {
+        deployToNexusRepository(false, additionalDeployArgs)
+    }
+
+    void deployToNexusRepository(Boolean useNexusStaging, String additionalDeployArgs = '') {
+        if (!deploymentRepository) {
+            script.error 'No deployment repository set. Cannot perform maven deploy.'
+        }
+
+        if (signatureCredentials) {
+            script.withCredentials([script.file(credentialsId: signatureCredentials.publicKeyRingFile, variable: 'pubring'),
+                                    script.file(credentialsId: signatureCredentials.secretKeyRingFile, variable: 'secring'),
+                                    script.string(credentialsId: signatureCredentials.secretKeyPassPhrase, variable: 'passphrase')
+            ]) {
+                additionalDeployArgs =
+                        // Sign jar using gpg
+                        "gpg:sign -Dgpg.publicKeyring=${script.env.publicKeyring} " +
+                                "-Dgpg.secretKeyring=${script.env.privateKeyring} " +
+                                "-Dgpg.passphrase=${script.env.passphrase} " +
+                                additionalDeployArgs
+
+                doDeployToNexusRepository(useNexusStaging, additionalDeployArgs)
+            }
+        } else {
+            script.echo 'No signature credentials set. Deploying unsigned'
+            doDeployToNexusRepository(useNexusStaging, additionalDeployArgs)
         }
     }
 
-    void deployToNexusRepository(Repository repository, String additionalDeployArgs = '') {
-        script.withCredentials([script.usernamePassword(credentialsId: repository.credentialsIdUsernameAndPassword,
+    void doDeployToNexusRepository(Boolean useNexusStaging, String additionalDeployArgs = '') {
+
+        String deployArgs = 'deploy'
+
+        if (useNexusStaging) {
+            // Use nexus-staging-maven-plugin instead of maven-deploy-plugin
+            // https://github.com/sonatype/nexus-maven-plugins/tree/master/staging/maven-plugin#maven2-only-or-explicit-maven3-mode
+            deployArgs =
+                    "org.sonatype.plugins:nexus-staging-maven-plugin:deploy -Dmaven.deploy.skip=true " +
+                    "-DserverId=${deploymentRepository.id} -DnexusUrl=${deploymentRepository.url} -DautoReleaseAfterClose=true "
+        }
+
+        script.withCredentials([script.usernamePassword(credentialsId: deploymentRepository.credentialsIdUsernameAndPassword,
                 passwordVariable: 'password', usernameVariable: 'username')]) {
 
-            String usernameProperty = "${repository.id}.username"
-            String passwordProperty = "${repository.id}.password"
+            String usernameProperty = "${deploymentRepository.id}.username"
+            String passwordProperty = "${deploymentRepository.id}.password"
 
             // The deploy plugin does not provide an option of passing server credentials via command line
             // So, create settings.xml that contains custom properties that can be set via command line (property
             // interpolation) - https://stackoverflow.com/a/28074776/1845976
-            writeSettingsXmlWithServer(repository.id,
-                    "\$${usernameProperty}",
-                    "\$${passwordProperty}")
+            writeSettingsXmlWithServer(deploymentRepository.id,
+                    "\${${usernameProperty}}",
+                    "\${${passwordProperty}}")
 
-            // TODO Is settings.xml picked up or do we need to use -s?
-
-            mvn "source:jar javadoc:jar deploy -DskipTests " +
+            mvn "source:jar javadoc:jar $deployArgs -DskipTests " +
                     // credentials for deploying to sonatype
-                    "-D${usernameProperty}=$username -D${passwordProperty}=$password " +
-                    "-DaltReleaseDeploymentRepository=${repository.id}::default::${repository.url}/nexus/content/repositories/releases/ " +
-                    "-DaltSnapshotDeploymentRepository=${repository.id}::default::${repository.url}/nexus/content/repositories/snapshots/ " +
+                    "-D${usernameProperty}=${script.env.username} -D${passwordProperty}=${script.env.password} " +
+                    "-DaltReleaseDeploymentRepository=${deploymentRepository.id}::default::${deploymentRepository.url}/nexus/content/repositories/releases/ " +
+                    "-DaltSnapshotDeploymentRepository=${deploymentRepository.id}::default::${deploymentRepository.url}/nexus/content/repositories/snapshots/ " +
                     additionalDeployArgs
         }
     }
@@ -114,7 +147,8 @@ abstract class Maven implements Serializable {
      * @param serverPassword
      */
     void writeSettingsXmlWithServer(def serverId, def serverUsername, def serverPassword) {
-        script.writeFile file: "${script.env.HOME}/.m2/settings.xml", text: """
+        script.echo "Writing ${script.pwd()}/.m2/settings.xml"
+        script.writeFile file: "${script.pwd()}/.m2/settings.xml", text: """
 <settings>
     <servers>
         <server>
@@ -126,7 +160,16 @@ abstract class Maven implements Serializable {
 </settings>"""
     }
 
+    // Unfortunately, inner classes cannot be accessed from Jenkinsfile: unable to resolve class Maven.Repository
+    // So we just use setters... See setDepoymentRepo()
     static class Repository implements Serializable {
+
+        Repository(String id, String url, String credentialsIdUsernameAndPassword) {
+            this.id = id
+            this.url = url
+            this.credentialsIdUsernameAndPassword = credentialsIdUsernameAndPassword
+        }
+
         String id
         String url
         String credentialsIdUsernameAndPassword
@@ -136,6 +179,13 @@ abstract class Maven implements Serializable {
      * Holds the Jenkins credentials IDs necessary for signing the jar.
      */
     static class SignatureCredentials implements Serializable {
+
+        SignatureCredentials(String publicKeyRingFile, String secretKeyRingFile, String secretKeyPassPhrase) {
+            this.publicKeyRingFile = publicKeyRingFile
+            this.secretKeyRingFile = secretKeyRingFile
+            this.secretKeyPassPhrase = secretKeyPassPhrase
+        }
+
         String publicKeyRingFile
         String secretKeyRingFile
         String secretKeyPassPhrase
