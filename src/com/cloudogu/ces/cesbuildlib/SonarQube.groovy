8dd237176ffba5cc10aa716121d49fe0baf62437
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2018-03-22 10:53:24 +0100

    SonarQube: Moves checking of SONAR_EXTRA_PROPS to withSonarQubeEnv{}.
    Otherwise they will always be null.

--- src/com/cloudogu/ces/cesbuildlib/SonarQube.groovy
+++ src/com/cloudogu/ces/cesbuildlib/SonarQube.groovy
@@ -33,12 +33,13 @@ class SonarQube implements Serializable {
      */
     void analyzeWith(Maven mvn) {
         initMaven(mvn)
-        String sonarExtraProps = script.env.SONAR_EXTRA_PROPS
-        if (sonarExtraProps == null) {
-            sonarExtraProps = ""
-        }
 
         script.withSonarQubeEnv(sonarQubeEnv) {
+            String sonarExtraProps = script.env.SONAR_EXTRA_PROPS
+            if (sonarExtraProps == null) {
+                sonarExtraProps = ""
+            }
+
             mvn "${script.env.SONAR_MAVEN_GOAL} -Dsonar.host.url=${script.env.SONAR_HOST_URL} " +
                     "-Dsonar.login=${script.env.SONAR_AUTH_TOKEN} ${sonarExtraProps}"
         }
