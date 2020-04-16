b32a9f2fc26c823c1c3f5a0704bf9ac509d27ae8
Robert Auer <robert.auer@cloudogu.com>
2020-04-16 07:33:20 +0200

    Use script from arguments and Sh object; #30

--- src/com/cloudogu/ces/cesbuildlib/Changelog.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Changelog.groovy
@@ -1,37 +1,32 @@
 package com.cloudogu.ces.cesbuildlib
 
 class Changelog implements Serializable {
+	private script
+	Sh sh
+
+	Git(script) {
+		this.script = script
+		this.sh = new Sh(script)
+	}
 
 	String getChangelog(String releaseVersion){
 	    def start = getChangelogStartIndex(releaseVersion)
 	    def end = getChangelogEndIndex(start)
-	    def output = sh (
-	        script: "sed '${start},${end}!d' CHANGELOG.md",
-	        returnStdout: true
-	    ).trim()
+	    def output = sh.returnStdOut("sed '${start},${end}!d' CHANGELOG.md").trim()
 	    return output.replace("\"", "").replace("'", "").replace("\\", "").replace	("\n", "\\n")
 	}
 
 	private int getChangelogStartIndex(String releaseVersion){
 	    def startLineString = "## \\[${releaseVersion}\\]"
-	    def script = "grep -n \"${startLineString}\" CHANGELOG.md | head -1 | sed s/\"^\\	([0-9]*\\)[:].*\$\"/\"\\1\"/g"
-	    def output = sh (
-	        script: script,
-	        returnStdout: true
-	    ).trim()
+	    def output = sh.returnStdOut("grep -n \"${startLineString}\" CHANGELOG.md | head -1 | sed s/\"^\\	([0-9]*\\)[:].*\$\"/\"\\1\"/g").trim()
 	    return (output as int) + 1
 	}
 
 	private String getChangelogEndIndex(int start){
-	    def script = "tail -n +${start+1} CHANGELOG.md |grep -n \"^## \\[.*\\]\" | sed s/	\"^\\([0-9]*\\)[:].*\$\"/\"\\1\"/g | head -1"
-	    def output = sh (
-	        script: script,
-	        returnStdout: true
-	    ).trim()
+	    def output = sh.returnStdOut("tail -n +${start+1} CHANGELOG.md |grep -n \"^## \\[.*\\]\" | sed s/	\"^\\([0-9]*\\)[:].*\$\"/\"\\1\"/g | head -1").trim()
 	    if ((output as String).length() > 0){
 	        return ((output as int) + start - 1) as String
 	    }
 	    return "\$"
 	}
-
 }
