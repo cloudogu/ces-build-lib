93ee850d15ee6a07222b61435f9f8e00b9b7f371
Robert Auer <robert.auer@cloudogu.com>
2020-04-16 08:11:09 +0200

    Define variables

--- src/com/cloudogu/ces/cesbuildlib/Git.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Git.groovy
@@ -147,15 +147,15 @@ class Git implements Serializable {
      * Creates a release on Github and fills it with the changes provided
      */
     void addGithubRelease(String releaseVersion, String changes){
-        branchName = getSimpleBranchName()
+        def branchName = getSimpleBranchName()
         if (credentials) {
             echo "Creating Github release..."
             script.withCredentials([script.usernamePassword(credentialsId: credentials, usernameVariable: 'GIT_AUTH_USR', passwordVariable: 'GIT_AUTH_PSW')]) {
-                body = "'{\"tag_name\": \"${releaseVersion}\", \"target_commitish\": \"master\", \"name\": \"${releaseVersion}\", \"body\":\"${changes}\"}'"
-                apiUrl = "https://api.github.com/repos/cloudogu/${branchName}/releases"
-                flags = "--request POST --data ${body} --header \"Content-Type: application/json\""
+                def body = "'{\"tag_name\": \"${releaseVersion}\", \"target_commitish\": \"master\", \"name\": \"${releaseVersion}\", \"body\":\"${changes}\"}'"
+                def apiUrl = "https://api.github.com/repos/cloudogu/${branchName}/releases"
+                def flags = "--request POST --data ${body} --header \"Content-Type: application/json\""
                 script = "curl -u ${GIT_AUTH_USR}:${GIT_AUTH_PSW} ${flags} ${apiUrl}"
-                output = script.sh (
+                def output = script.sh (
                     script: script,
                     returnStdout: true
                 ).trim()
