a74e9b03c51740c80980557009e8c2365a9bfd58
Simon Klein <simon.klein@cloudogu.com>
2020-05-18 08:30:08 +0200

    #30 Move finishGitRelease method to GitFlow class

--- src/com/cloudogu/ces/cesbuildlib/GitFlow.groovy
+++ src/com/cloudogu/ces/cesbuildlib/GitFlow.groovy
@@ -23,4 +23,59 @@ class GitFlow implements Serializable {
     boolean isReleaseBranch() {
         return this.git.getBranchName().startsWith("release/")
     }
+
+    /**
+     * Finishes a git flow release and pushes all merged branches to remote
+     *
+     * Only execute this function if you are already on a release branch
+     *
+     * @param releaseVersion the version that is going to be released
+     */
+    void finishGitRelease(String releaseVersion) {
+        String branchName = getBranchName()
+
+        // Check if tag already exists
+        if (this.git.tagExists("${releaseVersion}")) {
+            error("You cannot build this version, because it already exists.")
+        }
+
+        // Make sure all branches are fetched
+        script.sh "git config 'remote.origin.fetch' '+refs/heads/*:refs/remotes/origin/*'"
+        this.git.executeGitWithCredentials("fetch --all")
+
+        // Make sure there are no changes on develop
+        if (this.git.developHasChanged(branchName)) {
+            error("There are changes on develop branch that are not merged into release. Please merge and restart process.")
+        }
+
+        // Make sure any branch we need exists locally
+        script.sh "git checkout ${branchName}"
+        this.gitexecuteGitWithCredentials("pull origin ${branchName}")
+        script.sh "git checkout develop"
+        this.git.executeGitWithCredentials("pull origin develop")
+        script.sh "git checkout master"
+        this.git.executeGitWithCredentials("pull origin master")
+
+        // Merge release branch into master
+        script.sh "git merge --no-ff ${branchName}"
+
+        // Create tag. Use -f because the created tag will persist when build has failed.
+        this.git.executeGitWithCredentials("tag -f -m 'release version ${releaseVersion}' ${releaseVersion}")
+
+        // Merge release branch into develop
+        script.sh "git checkout develop"
+        script.sh "git merge --no-ff ${branchName}"
+
+        // Delete release branch
+        script.sh "git branch -d ${branchName}"
+
+        // Checkout tag
+        script.sh "git checkout ${releaseVersion}"
+
+        // Push changes and tags
+        this.git.executeGitWithCredentials("push origin master")
+        this.git.executeGitWithCredentials("push origin develop")
+        this.git.executeGitWithCredentials("push origin --tags")
+        this.git.executeGitWithCredentials("push origin --delete ${branchName}")
+    }
 }
