13a1d870f99d43d7f5dbefb995901e6d14be394b
Simon Klein <simon.klein@cloudogu.com>
2020-05-19 10:19:32 +0200

    #30 Fix failing tests

--- test/com/cloudogu/ces/cesbuildlib/GitHubTest.groovy
+++ test/com/cloudogu/ces/cesbuildlib/GitHubTest.groovy
@@ -32,15 +32,15 @@ class GitHubTest extends GroovyTestCase {
         assert scriptMock.actualGitArgs.branch == "gh-pages"
 
         assert scriptMock.actualDir == '.gh-pages'
-        assert scriptMock.actualShStringArgs.contains("cp -rf ../website/* ${subFolder}".toString())
-        assert scriptMock.actualShStringArgs.contains("mkdir -p ${subFolder}".toString())
-        assert scriptMock.actualShStringArgs.contains('git add .')
-        assert scriptMock.actualShStringArgs.contains('git commit -m "Deploys new version of website"')
+        assert scriptMock.allActualArgs.contains("cp -rf ../website/* ${subFolder}".toString())
+        assert scriptMock.allActualArgs.contains("mkdir -p ${subFolder}".toString())
+        assert scriptMock.allActualArgs.contains('git add .')
+        assert scriptMock.allActualArgs.contains('git commit -m "Deploys new version of website"')
         assert scriptMock.actualWithEnv.contains("${'GIT_AUTHOR_NAME=User Name'}")
         assert scriptMock.actualWithEnv.contains("${'GIT_COMMITTER_NAME=User Name'}")
         assert scriptMock.actualWithEnv.contains("${'GIT_AUTHOR_EMAIL=user.name@doma.in'}")
         assert scriptMock.actualWithEnv.contains("${'GIT_COMMITTER_EMAIL=user.name@doma.in'}")
-        assert scriptMock.actualShStringArgs.contains('git push origin gh-pages')
-        assert scriptMock.actualShStringArgs.last == 'rm -rf .gh-pages'
+        assert scriptMock.allActualArgs.contains('git push origin gh-pages')
+        assert scriptMock.allActualArgs.last == 'rm -rf .gh-pages'
     }
 }
