02d0f4079e7d8f60df896087a6a2d89aeb25bffd
Simon Klein <simon.klein@cloudogu.com>
2020-05-25 08:21:26 +0200

    #30 Add comments

--- src/com/cloudogu/ces/cesbuildlib/Changelog.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Changelog.groovy
@@ -1,5 +1,8 @@
 package com.cloudogu.ces.cesbuildlib
 
+/**
+ * Parse a changelog that is based on the changelog format on https://keepachangelog.com/
+ */
 class Changelog implements Serializable {
     private script
     private String changelog
@@ -17,10 +20,19 @@ class Changelog implements Serializable {
         this.changelog = changelog
     }
 
+    /**
+     * @return Returns the content of the given changelog.
+     */
     private String getChangelog(){
         this.sh.returnStdOut("cat ${changelog}")
     }
 
+    /**
+     * Extracts the changes for a given version out of the changelog.
+     *
+     * @param releaseVersion The version to get the changes for.
+     * @return Returns the changes as String.
+     */
     String getChangesForVersion(String releaseVersion) {
         def start = getChangelogStartIndex(releaseVersion)
         if (start == -1){
@@ -31,6 +43,12 @@ class Changelog implements Serializable {
         return formatForJson(getChangelog().substring(start, end).trim())
     }
 
+    /**
+     * Removes characters from a string that could break the json struct when passing the string as json value.
+     *
+     * @param string The string to format.
+     * @return Returns the formatted string.
+     */
     String formatForJson(String string) {
         return string
                 .replace("\"", "")
@@ -39,11 +57,23 @@ class Changelog implements Serializable {
                 .replace("\n", "\\n")
     }
 
+    /**
+     * Returns the start index of changes of a specific release version in the changelog.
+     *
+     * @param releaseVersion The version to get the changes for.
+     * @return Returns the index in the changelog string where the changes start.
+     */
     private int getChangelogStartIndex(String releaseVersion) {
         int offset = Math.min("## [${releaseVersion}]".length(), 0)
         return getChangelog().indexOf("## [${releaseVersion}]") + offset
     }
 
+    /**
+     * Returns the end index of changes of a specific release version in the changelog.
+     *
+     * @param start The start index of the changes for this version.
+     * @return Returns the index in the changelog string where the changes end.
+     */
     private int getChangelogEndIndex(int start) {
         def changelogAfterStartIndex = getChangelog().substring(start)
         def index = changelogAfterStartIndex.indexOf("\n## [")
