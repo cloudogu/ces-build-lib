d4d44462c44a6eedcdebbffb3756a18b91e2e6b0
Philipp Markiewka <philipp.markiewka@cloudogu.com>
2021-01-21 21:51:03 +0100

    Refactors credentials handling and naming
    Signed-off-by: Philipp Markiewka <philipp.markiewka@cloudogu.com>

--- src/com/cloudogu/ces/cesbuildlib/SCMManager.groovy
+++ src/com/cloudogu/ces/cesbuildlib/SCMManager.groovy
@@ -25,10 +25,13 @@ class SCMManager implements Serializable{
   }
 
   protected String getCurlAuthParam() {
-    if(script.env.GIT_USER && script.env.GIT_PASSWORD) {
-      return "-u ${script.env.GIT_USER}:${script.env.GIT_PASSWORD}"
+    String curlAuthParam = ""
+    if(credentials) {
+      executeWithCredentials {
+        curlAuthParam = "-u ${script.env.GIT_USER}:${script.env.GIT_PASSWORD}"
+      }
     }
-    return ""
+    return curlAuthParam
   }
 
   Object searchPullRequestByTitle(String title) {
@@ -43,55 +46,50 @@ class SCMManager implements Serializable{
   }
 
   Object getPullRequests() {
-    String httpResonse = ""
-    executeWithCredentials {
-      String script = """curl -w "%{http_code}" ${getCurlAuthParam()} -H 'Content-Type: application/vnd.scmm-pullRequestCollection+json;v=2' https://${this.repositoryUrl}"""
-      httpResonse = this.script.sh returnStdout: true, script: script
-    }
+    String curlCommand = """curl -w "%{http_code}" ${getCurlAuthParam()} -H 'Content-Type: application/vnd.scmm-pullRequestCollection+json;v=2' https://${this.repositoryUrl}"""
+    String httpResponse = script.sh returnStdout: true, script: curlCommand
+
     //get only the http code (3 characters) of the response.
-    String http_code = httpResonse.reverse().take(3).reverse()
+    String httpCode = httpResponse.reverse().take(3).reverse()
     //drop the http code (last 3 characters) of this string
-    String pullRequests = httpResonse.reverse().drop(3).reverse()
+    String pullRequests = httpResponse.reverse().drop(3).reverse()
 
-    http_code = http_code.trim()
-    this.script.echo "Getting all pull requests yields http_code: ${http_code}"
-    if (http_code != "200") {
-      unstable 'Could not create pull request'
+    httpCode = httpCode.trim()
+    script.echo "Getting all pull requests yields httpCode: ${httpCode}"
+    if (httpCode != "200") {
+      script.unstable 'Could not create pull request'
     }
 
-    def prsAsJson = this.script.readJSON text: pullRequests
+    def prsAsJson = script.readJSON text: pullRequests
     return prsAsJson["_embedded"]["pullRequests"]
   }
 
   String createPullRequest(String source, String target, String title, String description) {
-    def data = """{"title": "${title}", "description": "${description}", "source": "${source}", "target": "${target}"}"""
+    String data = """{"title": "${title}", "description": "${description}", "source": "${source}", "target": "${target}"}"""
+    String curlCommand = """curl -i -X POST ${getCurlAuthParam()} -H 'Content-Type: application/vnd.scmm-pullRequest+json;v=2' -d '${data}' https://${this.repositoryUrl}"""
+    String header = script.sh returnStdout: true, script: curlCommand
 
-    String header = ""
-    executeWithCredentials { auth ->
-      def script = """curl -i -X POST ${getCurlAuthParam()} -H 'Content-Type: application/vnd.scmm-pullRequest+json;v=2' -d '${data}' https://${this.repositoryUrl}"""
-      header = this.script.sh returnStdout: true, script: script
-    }
     String[] splitHeader = header.split("\n")
 
-    String http_code
+    String httpCode = ""
     for(String line: splitHeader) {
       if(line.contains("HTTP/2")){
-        String[] http_code_full = line.split(" ")
-        http_code = http_code_full[1]
+        String[] httpCodeFull = line.split(" ")
+        httpCode = httpCodeFull[1]
       }
     }
 
-    http_code = http_code.trim()
-    this.script.echo "Creating pull request yields http_code: ${http_code}"
-    if (http_code != "201") {
-      unstable 'Could not create pull request'
+    httpCode = httpCode.trim()
+    script.echo "Creating pull request yields httpCode: ${httpCode}"
+    if (httpCode != "201") {
+      script.unstable 'Could not create pull request'
     }
 
-    String pullRequestId
+    String pullRequestId = ""
     for(String line: splitHeader) {
       if(line.contains("location:")){
-        String[] http_code_full = line.split("/")
-        pullRequestId = http_code_full[http_code_full.size() - 1]
+        String[] httpCodeFull = line.split("/")
+        pullRequestId = httpCodeFull[httpCodeFull.size() - 1]
         pullRequestId.trim()
       }
     }
@@ -101,33 +99,26 @@ class SCMManager implements Serializable{
 
   void updateDescription(String pullRequestId, String title, String description) {
     // In order to update the description put in also the title. Otherwise the title is overwritten with an empty string.
-    def data = """{"title": "${title}","description": "${description}"}"""
-    String http_code = ""
-    executeWithCredentials { auth ->
-      def script = """curl -X PUT -w "%{http_code}" ${getCurlAuthParam()} -H 'Content-Type: application/vnd.scmm-pullRequest+json;v=2' -d '${data}' https://${this.repositoryUrl}/${pullRequestId}"""
-      http_code = this.script.sh returnStdout: true, script: script
-    }
+    String data = """{"title": "${title}","description": "${description}"}"""
+    String curlCommand = """curl -X PUT -w "%{http_code}" ${getCurlAuthParam()} -H 'Content-Type: application/vnd.scmm-pullRequest+json;v=2' -d '${data}' https://${this.repositoryUrl}/${pullRequestId}"""
+    String httpCode = script.sh returnStdout: true, script: curlCommand
 
-    http_code = http_code.trim()
-    this.script.echo "Description update yields http_code: ${http_code}"
-    if (http_code != "204") {
-      unstable 'Could not update description'
+    httpCode = httpCode.trim()
+    script.echo "Description update yields http_code: ${httpCode}"
+    if (httpCode != "204") {
+      script.unstable 'Could not update description'
     }
   }
 
   void addComment(String pullRequestId, String comment) {
-    def data = """{"comment": "${comment}"}"""
+    String data = """{"comment": "${comment}"}"""
+    String curlCommand = """curl -X POST -w "%{http_code}" ${getCurlAuthParam()} -H 'Content-Type: application/json' -d '${data}' https://${this.repositoryUrl}/${pullRequestId}/comments"""
+    String httpCode = script.sh returnStdout: true, script: curlCommand
 
-    String http_code = ""
-    executeWithCredentials { auth ->
-      def script = """curl -X POST -w "%{http_code}" ${getCurlAuthParam()} -H 'Content-Type: application/json' -d '${data}' https://${this.repositoryUrl}/${pullRequestId}/comments"""
-      http_code = this.script.sh returnStdout: true, script: script
-    }
-
-    http_code = http_code.trim()
-    this.script.echo "Adding comment yields http_code: ${http_code}"
-    if (http_code != "201") {
-      unstable 'Could not add comment'
+    httpCode = httpCode.trim()
+    script.echo "Adding comment yields http_code: ${httpCode}"
+    if (httpCode != "201") {
+      script.unstable 'Could not add comment'
     }
   }
 
