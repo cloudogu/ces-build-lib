5bf1c43affd5065dcfb6d27abd2afe9227529fba
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2021-01-27 15:27:27 +0100

    Format SCMManager like other files and add .editorconfig

--- src/com/cloudogu/ces/cesbuildlib/SCMManager.groovy
+++ src/com/cloudogu/ces/cesbuildlib/SCMManager.groovy
@@ -2,115 +2,115 @@ package com.cloudogu.ces.cesbuildlib
 
 import groovy.json.JsonOutput
 
-class SCMManager implements Serializable{
+class SCMManager implements Serializable {
 
-  private script
-  private HttpClient http
-  String repositoryUrl
+    private script
+    private HttpClient http
+    String repositoryUrl
 
-  SCMManager(script, credentials) {
-    this.script = script
-    this.http = new HttpClient(script, credentials)  
-  }
-
-  String searchPullRequestIdByTitle(String title) {
-    def pullRequest
-    for (Map pr : getPullRequests()) {
-      if (pr.title == title) {
-        pullRequest = pr
-      }
-    }
-    
-    if (pullRequest) {
-      return pullRequest.id.toString()
-    } else {
-      return ""
-    }
-  }
-
-  String createPullRequest(String source, String target, String title, String description) {
-    def dataJson = JsonOutput.toJson([
-          title: title,
-          description: description,
-          source: source,
-          target: target
-    ])
-    def httpResponse = http.post("https://${this.repositoryUrl}", 'application/vnd.scmm-pullRequest+json;v=2', dataJson)
-
-    script.echo "Creating pull request yields httpCode: ${httpResponse.httpCode}"
-    if (httpResponse.httpCode != "201") {
-      script.echo 'WARNING: Http status code indicates, that pull request was not created'
-      script.unstable 'Could not create pull request'
-      return ''
+    SCMManager(script, credentials) {
+        this.script = script
+        this.http = new HttpClient(script, credentials)
     }
 
-    // example: "location: https://some/pr/42" - extract ide
-    return httpResponse.headers.location.split("/")[-1]
-  }
+    String searchPullRequestIdByTitle(String title) {
+        def pullRequest
+        for (Map pr : getPullRequests()) {
+            if (pr.title == title) {
+                pullRequest = pr
+            }
+        }
 
-  void updateDescription(String pullRequestId, String title, String description) {
-    // In order to update the description put in also the title. Otherwise the title is overwritten with an empty string.
-    def dataJson = JsonOutput.toJson([
-          title: title,
-          description: description
-    ])
-
-   def httpResponse = http.put("https://${this.repositoryUrl}/${pullRequestId}", 'application/vnd.scmm-pullRequest+json;v=2', dataJson)
-
-    script.echo "Description update yields http_code: ${httpResponse.httpCode}"
-    if (httpResponse.httpCode != "204") {
-      script.unstable 'Could not update description'
-    }
-  }
-
-  void addComment(String pullRequestId, String comment) {
-    def dataJson = JsonOutput.toJson([
-          comment: comment
-    ])
-    def httpResponse = http.post("https://${this.repositoryUrl}/${pullRequestId}/comments", 'application/json', dataJson)
-
-    script.echo "Adding comment yields http_code: ${httpResponse.httpCode}"
-    if (httpResponse.httpCode != "201") {
-      script.unstable 'Could not add comment'
-    }
-  }
-
-  /**
-    * @return SCM-Manager's representation of PRs. Basically a list of PR objects.
-    *  properties (as of SCM-Manager 2.12.0)
-    *  * id
-    *  * author
-    *    * id
-    *    * displayName
-    *    * mail
-    *  * source - the source branch
-    *  * target - the target branch
-    *  * title 
-    *  * description (branch)
-    *  * creationDate: (e.g. "2020-10-09T15:08:11.459Z")
-    *  * lastModified"
-    *  * status, e.g. "OPEN"
-    *  * reviewer (list)
-    *  * tasks
-    *    * todo (number)
-    *    * done (number
-    *  * tasks sourceRevision
-    *  * targetRevision
-    *  * targetRevision 
-    *  * markedAsReviewed (list)
-    *  * emergencyMerged
-    *  * ignoredMergeObstacles
-    */  
-  protected getPullRequests() {
-    def httpResponse = http.get("https://${this.repositoryUrl}", 'application/vnd.scmm-pullRequestCollection+json;v=2')
-    
-    script.echo "Getting all pull requests yields httpCode: ${httpResponse.httpCode}"
-    if (httpResponse.httpCode != "200") {
-      script.unstable 'Could not create pull request'
+        if (pullRequest) {
+            return pullRequest.id.toString()
+        } else {
+            return ""
+        }
     }
 
-    def prsAsJson = script.readJSON text: httpResponse.body
-    return prsAsJson._embedded.pullRequests
-  }
+    String createPullRequest(String source, String target, String title, String description) {
+        def dataJson = JsonOutput.toJson([
+            title      : title,
+            description: description,
+            source     : source,
+            target     : target
+        ])
+        def httpResponse = http.post("https://${this.repositoryUrl}", 'application/vnd.scmm-pullRequest+json;v=2', dataJson)
+
+        script.echo "Creating pull request yields httpCode: ${httpResponse.httpCode}"
+        if (httpResponse.httpCode != "201") {
+            script.echo 'WARNING: Http status code indicates, that pull request was not created'
+            script.unstable 'Could not create pull request'
+            return ''
+        }
+
+        // example: "location: https://some/pr/42" - extract ide
+        return httpResponse.headers.location.split("/")[-1]
+    }
+
+    void updateDescription(String pullRequestId, String title, String description) {
+        // In order to update the description put in also the title. Otherwise the title is overwritten with an empty string.
+        def dataJson = JsonOutput.toJson([
+            title      : title,
+            description: description
+        ])
+
+        def httpResponse = http.put("https://${this.repositoryUrl}/${pullRequestId}", 'application/vnd.scmm-pullRequest+json;v=2', dataJson)
+
+        script.echo "Description update yields http_code: ${httpResponse.httpCode}"
+        if (httpResponse.httpCode != "204") {
+            script.unstable 'Could not update description'
+        }
+    }
+
+    void addComment(String pullRequestId, String comment) {
+        def dataJson = JsonOutput.toJson([
+            comment: comment
+        ])
+        def httpResponse = http.post("https://${this.repositoryUrl}/${pullRequestId}/comments", 'application/json', dataJson)
+
+        script.echo "Adding comment yields http_code: ${httpResponse.httpCode}"
+        if (httpResponse.httpCode != "201") {
+            script.unstable 'Could not add comment'
+        }
+    }
+
+    /**
+     * @return SCM-Manager's representation of PRs. Basically a list of PR objects.
+     *  properties (as of SCM-Manager 2.12.0)
+     *  * id
+     *  * author
+     *    * id
+     *    * displayName
+     *    * mail
+     *  * source - the source branch
+     *  * target - the target branch
+     *  * title 
+     *  * description (branch)
+     *  * creationDate: (e.g. "2020-10-09T15:08:11.459Z")
+     *  * lastModified"
+     *  * status, e.g. "OPEN"
+     *  * reviewer (list)
+     *  * tasks
+     *    * todo (number)
+     *    * done (number
+     *  * tasks sourceRevision
+     *  * targetRevision
+     *  * targetRevision 
+     *  * markedAsReviewed (list)
+     *  * emergencyMerged
+     *  * ignoredMergeObstacles
+     */
+    protected getPullRequests() {
+        def httpResponse = http.get("https://${this.repositoryUrl}", 'application/vnd.scmm-pullRequestCollection+json;v=2')
+
+        script.echo "Getting all pull requests yields httpCode: ${httpResponse.httpCode}"
+        if (httpResponse.httpCode != "200") {
+            script.unstable 'Could not create pull request'
+        }
+
+        def prsAsJson = script.readJSON text: httpResponse.body
+        return prsAsJson._embedded.pullRequests
+    }
 
 }
--- test/com/cloudogu/ces/cesbuildlib/SCMManagerTest.groovy
+++ test/com/cloudogu/ces/cesbuildlib/SCMManagerTest.groovy
@@ -3,23 +3,23 @@ package com.cloudogu.ces.cesbuildlib
 import org.junit.Before
 import org.junit.Test
 
-import static org.assertj.core.api.Assertions.assertThat 
+import static org.assertj.core.api.Assertions.assertThat
 
 class SCMManagerTest {
 
-  ScriptMock scriptMock = new ScriptMock()
-  SCMManager scmm = new SCMManager(scriptMock, "credentialsID")
+    ScriptMock scriptMock = new ScriptMock()
+    SCMManager scmm = new SCMManager(scriptMock, "credentialsID")
 
-  @Before
-  void init() {
-    scmm.repositoryUrl = "scm/repo"
-    scriptMock.env.put("GIT_USER", "user")
-    scriptMock.env.put("GIT_PASSWORD", "pw")
-  }
+    @Before
+    void init() {
+        scmm.repositoryUrl = "scm/repo"
+        scriptMock.env.put("GIT_USER", "user")
+        scriptMock.env.put("GIT_PASSWORD", "pw")
+    }
 
-  @Test
-  void "getting all pull requests"() {
-    String response = """HTTP/2 200
+    @Test
+    void "getting all pull requests"() {
+        String response = """HTTP/2 200
 some: header
 
 {
@@ -34,15 +34,15 @@ some: header
     ]
   }
 }"""
-    scriptMock.expectedDefaultShRetValue = response
-    def prs = scmm.getPullRequests()
-    def id = prs["id"]
-    assertThat(id.toString()).isEqualTo('[1, 2]')
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        def prs = scmm.getPullRequests()
+        def id = prs["id"]
+        assertThat(id.toString()).isEqualTo('[1, 2]')
+    }
 
-  @Test
-  void "server error yields to unstable build while getting all pull requests"() {
-    def response = """HTTP/2 500
+    @Test
+    void "server error yields to unstable build while getting all pull requests"() {
+        def response = """HTTP/2 500
 some: header
 
 {
@@ -57,14 +57,14 @@ some: header
     ]
   }
 }"""
-    scriptMock.expectedDefaultShRetValue = response
-    scmm.getPullRequests()
-    assertThat(scriptMock.unstable)
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        scmm.getPullRequests()
+        assertThat(scriptMock.unstable)
+    }
 
-  @Test
-  void "find pull request by title"() {
-    def response = """HTTP/2 200
+    @Test
+    void "find pull request by title"() {
+        def response = """HTTP/2 200
 some: header
 
 {
@@ -81,14 +81,14 @@ some: header
     ]
   }
 }"""
-    scriptMock.expectedDefaultShRetValue = response
-    def prs = scmm.searchPullRequestIdByTitle("one")
-    assertThat(prs).isEqualTo('1')
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        def prs = scmm.searchPullRequestIdByTitle("one")
+        assertThat(prs).isEqualTo('1')
+    }
 
-  @Test
-  void "did not find pull request by title"() {
-    def response = """HTTP/2 200
+    @Test
+    void "did not find pull request by title"() {
+        def response = """HTTP/2 200
 some: header
 
 {
@@ -103,14 +103,14 @@ some: header
     ]
   }
 }"""
-    scriptMock.expectedDefaultShRetValue = response
-    def prs = scmm.searchPullRequestIdByTitle("3")
-    assertThat(prs).isEqualTo("")
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        def prs = scmm.searchPullRequestIdByTitle("3")
+        assertThat(prs).isEqualTo("")
+    }
 
-  @Test
-  void "returns empty string when no pr is found"() {
-    def response = """HTTP/2 200
+    @Test
+    void "returns empty string when no pr is found"() {
+        def response = """HTTP/2 200
 some: header
 
 {
@@ -118,71 +118,71 @@ some: header
     "pullRequests": []
   }
 }"""
-    scriptMock.expectedDefaultShRetValue = response
-    def prs = scmm.searchPullRequestIdByTitle("just something")
-    assertThat(prs).isEqualTo("")
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        def prs = scmm.searchPullRequestIdByTitle("just something")
+        assertThat(prs).isEqualTo("")
+    }
 
-  @Test
-  void "sucessfully creating a pull request yields the created prs id"() {
-    def response = """
+    @Test
+    void "sucessfully creating a pull request yields the created prs id"() {
+        def response = """
 HTTP/2 201
 location: https://a/long/url/with/id/id/12
 """
-    scriptMock.expectedDefaultShRetValue = response
-    def id = scmm.createPullRequest("source", "target", "title", "description")
-    assertThat(id.toString()).isEqualTo("12")
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        def id = scmm.createPullRequest("source", "target", "title", "description")
+        assertThat(id.toString()).isEqualTo("12")
+    }
 
-  @Test
-  void "error on pull request creation makes build unstable"() {
-    def response = """
+    @Test
+    void "error on pull request creation makes build unstable"() {
+        def response = """
 HTTP/2 500
 no-location: https://a/long/url/with/id/12
 """
-    scriptMock.expectedDefaultShRetValue = response
-    def id = scmm.createPullRequest("source","target","title","description")
-    assertThat(id.toString()).isEqualTo("")
-    assertThat(scriptMock.unstable)
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        def id = scmm.createPullRequest("source", "target", "title", "description")
+        assertThat(id.toString()).isEqualTo("")
+        assertThat(scriptMock.unstable)
+    }
 
-  @Test
-  void "successful description update yields to a successful build"() {
-    def response = """HTTP/2 200
+    @Test
+    void "successful description update yields to a successful build"() {
+        def response = """HTTP/2 200
 some: header
 """
-    scriptMock.expectedDefaultShRetValue = response
-    scmm.updateDescription("123","title","description")
-    assertThat(!scriptMock.unstable)
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        scmm.updateDescription("123", "title", "description")
+        assertThat(!scriptMock.unstable)
+    }
 
-  @Test
-  void "error on description update yields to an unstable build"() {
-    def response = """HTTP/2 200
+    @Test
+    void "error on description update yields to an unstable build"() {
+        def response = """HTTP/2 200
 some: header
 """
-    scriptMock.expectedDefaultShRetValue = response
-    scmm.updateDescription("123","title","description")
-    assertThat(scriptMock.unstable)
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        scmm.updateDescription("123", "title", "description")
+        assertThat(scriptMock.unstable)
+    }
 
-  @Test
-  void "successful comment update yields to a successful build"() {
-      def response = """HTTP/2 201
+    @Test
+    void "successful comment update yields to a successful build"() {
+        def response = """HTTP/2 201
 some: header
 """
-    scriptMock.expectedDefaultShRetValue = response
-    scmm.addComment("123","comment")
-    assertThat(!scriptMock.unstable)
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        scmm.addComment("123", "comment")
+        assertThat(!scriptMock.unstable)
+    }
 
-  @Test
-  void "error on comment update yields to an unstable build"() {
-      def response = """HTTP/2 500
+    @Test
+    void "error on comment update yields to an unstable build"() {
+        def response = """HTTP/2 500
 some: header
 """
-    scriptMock.expectedDefaultShRetValue = response
-    scmm.addComment("123","comment")
-    assertThat(scriptMock.unstable)
-  }
+        scriptMock.expectedDefaultShRetValue = response
+        scmm.addComment("123", "comment")
+        assertThat(scriptMock.unstable)
+    }
 }
