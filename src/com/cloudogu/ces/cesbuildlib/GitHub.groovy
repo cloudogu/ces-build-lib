6a050e5aa746949a8986b1c62088ac84ed517348
Simon Klein <simon.klein@cloudogu.com>
2021-09-09 12:09:32 +0200

    #64 Update GitHub

--- src/com/cloudogu/ces/cesbuildlib/GitHub.groovy
+++ src/com/cloudogu/ces/cesbuildlib/GitHub.groovy
@@ -1,25 +1,34 @@
 package com.cloudogu.ces.cesbuildlib
 
+import groovy.json.JsonSlurper
+
 class GitHub implements Serializable {
     private script
     private git
+    Sh sh
 
     GitHub(script, git) {
         this.script = script
         this.git = git
+        this.sh = new Sh(script)
     }
 
-    void addReleaseAsset(String releaseId, String filePath){
+    void addReleaseAsset(String releaseId, String filePath) {
         def repositoryName = git.getRepositoryName()
 
-        script.withCredentials([script.usernamePassword(
-            credentialsId: git.credentials, usernameVariable: 'GIT_AUTH_USR', passwordVariable: 'GIT_AUTH_PSW')]) {
+        try {
+            script.withCredentials([script.usernamePassword(
+                credentialsId: git.credentials, usernameVariable: 'GIT_AUTH_USR', passwordVariable: 'GIT_AUTH_PSW')]) {
 
-            def apiUrl = "https://uploads.github.com/repos/${repositoryName}/releases/${releaseId}/assets?name=\$(basename ${filePath})}"
-            def flags = """--header "Content-Type: multipart/form-data" --data-binary @${filePath}}"""
-            def username = '\$GIT_AUTH_USR'
-            def password = '\$GIT_AUTH_PSW'
-            script.sh.returnStdOut "curl -u ${username}:${password} ${flags} ${apiUrl}"
+                def apiUrl = "https://uploads.github.com/repos/${repositoryName}/releases/${releaseId}/assets?name=\$(basename ${filePath})"
+                def flags = """--header "Content-Type: multipart/form-data" --data-binary @${filePath}"""
+                def username = '\$GIT_AUTH_USR'
+                def password = '\$GIT_AUTH_PSW'
+                this.sh.returnStdOut "curl -u ${username}:${password} ${flags} ${apiUrl}"
+            }
+        } catch (Exception e) {
+            script.unstable("Asset upload failed due to error: ${e}")
+            script.echo 'Please manually upload asset.'
         }
     }
 
@@ -33,11 +42,10 @@ class GitHub implements Serializable {
         try {
             def changelogText = changelog.changesForVersion(releaseVersion)
             script.echo "The description of github release will be: >>>${changelogText}<<<"
-            return createRelease(releaseVersion, changelogText, productionBranch)
+            createRelease(releaseVersion, changelogText, productionBranch)
         } catch (IllegalArgumentException e) {
             script.unstable("Release failed due to error: ${e}")
             script.echo 'Please manually update github release.'
-            return ""
         }
     }
 
@@ -50,15 +58,17 @@ class GitHub implements Serializable {
             throw new IllegalArgumentException('Unable to create Github release without credentials.')
         }
         script.withCredentials([script.usernamePassword(
-                credentialsId: git.credentials, usernameVariable: 'GIT_AUTH_USR', passwordVariable: 'GIT_AUTH_PSW')]) {
-            
-            def body = 
-                    """{"tag_name": "${releaseVersion}", "target_commitish": "${productionBranch}", "name": "${releaseVersion}", "body":"${changes}"}"""
+            credentialsId: git.credentials, usernameVariable: 'GIT_AUTH_USR', passwordVariable: 'GIT_AUTH_PSW')]) {
+
+            def body =
+                """{"tag_name": "${releaseVersion}", "target_commitish": "${productionBranch}", "name": "${releaseVersion}", "body":"${changes}"}"""
             def apiUrl = "https://api.github.com/repos/${repositoryName}/releases"
             def flags = """--request POST --data '${body.trim()}' --header "Content-Type: application/json" """
             def username = '\$GIT_AUTH_USR'
             def password = '\$GIT_AUTH_PSW'
-            script.sh.returnStdOut "curl -u ${username}:${password} ${flags} ${apiUrl}"
+            def var=this.sh.returnStdOut("curl -u ${username}:${password} ${flags} ${apiUrl}")
+            def jsonSlurper = new JsonSlurper()
+            return jsonSlurper.parseText(var).id
         }
     }
 
