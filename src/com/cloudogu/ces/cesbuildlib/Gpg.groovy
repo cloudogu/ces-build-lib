502f1b3fe4184f2f4c3e50c68213459777e3a7eb
Simon Klein <simon.klein@cloudogu.com>
2021-09-13 13:29:02 +0200

    #64 Update gpg class

--- src/com/cloudogu/ces/cesbuildlib/Gpg.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Gpg.groovy
@@ -2,36 +2,48 @@ package com.cloudogu.ces.cesbuildlib
 
 class Gpg {
     private script
+    private docker
 
     Gpg(script) {
         this.script = script
+        this.docker = new Docker(script)
     }
 
-    public void createSignature(){
+    /**
+     * Executes the 'make signature-ci' command to create the digital signature for a build.
+     */
+    void createSignature() {
         this.withPrivateKey {
-            script.sh ("make passphrase=${passphrase} signature-ci") // The passphrase variable is provided in `withPrivateKey`
+            // The passphrase variable is provided in `withPrivateKey`
+            script.sh("make passphrase=\$passphrase signature-ci")
         }
     }
 
-    private void withGpg(Closure closure){
-        withImage{
-            String dockerArgs = "-v ${WORKSPACE}:/tmp/workspace"
-            dockerArgs <<= " --entrypoint=''"
-            dockerArgs <<= " -v ${pwd}/.gnupg:/root/.gnupg"
-            new Docker(this)
-                .image('cloudogu/gpg:1.0')
-                .mountJenkinsUser()
-                .inside(dockerArgs){
-                    script.sh "cd /tmp/workspace"
-                    closure.call()
-                }
-        }
+    /**
+     * Calls a closure inside of a docker container which is able to execute 'gpg' commands as well as 'make' commands.
+     * @param closure The closure which is called inside of the docker container.
+     */
+    private void withGpg(Closure closure) {
+        this.buildGpgDockerImage()
+        String dockerArgs = "-v ${script.env.WORKSPACE}:/tmp/workspace"
+        dockerArgs <<= " --entrypoint=''"
+        dockerArgs <<= " -v ${script.env.pwd}/.gnupg:/root/.gnupg"
+        docker
+            .image('cloudogu/gpg:1.0')
+            .mountJenkinsUser()
+            .inside(dockerArgs) {
+                script.sh "cd /tmp/workspace"
+                closure.call()
+            }
     }
 
-    private void withImage(Closure closure){
-        dockerfile = """
+    /**
+     * Creates, builds and then removes a Dockerfile which is able to execute 'gpg' commands as well as 'make' commands.
+     */
+    private void buildGpgDockerImage() {
+        def dockerfile = """
         FROM debian:stable-slim
-        LABEL maintainer="simon.klein@cloudogu.com"
+        LABEL maintainer="hello@cloudogu.com"
 
         RUN apt update && apt install -y gnupg2 make git
 
@@ -39,9 +51,8 @@ class Gpg {
     """
         try {
             script.writeFile encoding: 'UTF-8', file: 'Dockerfile.gpgbuild', text: dockerfile.trim()
-            new Docker(this).build("cloudogu/gpg:1.0", "-f Dockerfile.gpgbuild .")
-            closure.call()
-        } catch(e){
+            docker.build("cloudogu/gpg:1.0", "-f Dockerfile.gpgbuild .")
+        } catch (e) {
             script.echo "${e}"
             throw e
         } finally {
@@ -49,7 +60,11 @@ class Gpg {
         }
     }
 
-    private void withPrivateKey(Closure closure){
+    /**
+     * Calls a closure in an environment which contains a gpg signing key and is able to execute 'gpg' commands and 'make' commands.
+     * @param closure The closure to call.
+     */
+    private void withPrivateKey(Closure closure) {
         script.withCredentials([script.string(credentialsId: 'jenkins_gpg_private_key_passphrase', variable: 'passphrase')]) {
             script.withCredentials([script.file(credentialsId: 'jenkins_gpg_private_key_for_ces_tool_release_signing', variable: 'pkey')]) {
                 try {
@@ -57,7 +72,7 @@ class Gpg {
                         script.sh "gpg --yes --always-trust --pinentry-mode loopback --passphrase=\"\$passphrase\" --import \$pkey"
                         closure.call()
                     }
-                } catch(e){
+                } catch (e) {
                     script.echo "${e}"
                     throw e
                 }
