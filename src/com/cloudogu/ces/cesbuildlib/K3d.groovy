f64aaa203ee29163049a15d3beb849cd993ba8dd
Robert Auer <robert.auer@cloudogu.com>
2022-03-01 09:41:34 +0100

    Use correct environment variables; #67

--- src/com/cloudogu/ces/cesbuildlib/K3d.groovy
+++ src/com/cloudogu/ces/cesbuildlib/K3d.groovy
@@ -4,13 +4,13 @@ class K3d {
     private String workspace
     private String clusterName
     private script
-    Sh sh
+    private String path
 
-    K3d(String workspace, String clusterName) {
+    K3d(String workspace, String clusterName, script, String path) {
         this.workspace = workspace
         this.clusterName = clusterName
         this.script = script
-        this.sh = new Sh(script)
+        this.path = path
     }
 
     /**
@@ -27,22 +27,21 @@ class K3d {
     /**
      * Starts a k3d cluster in Docker
      * Utilizes code from the cloudogu/gitops-playground
-     *
-     * @param clusterName The name your cluster should get
      */
-    void startK3d(String clusterName) {
-        sh.returnStdOut "mkdir -p ${this.workspace}/.k3d/bin"
+    void startK3d() {
+        script.sh "mkdir -p ${workspace}/.k3d/bin"
 
-        git branch: 'main', url: 'https://github.com/cloudogu/gitops-playground'
+        script.git branch: 'main', url: 'https://github.com/cloudogu/gitops-playground'
 
-        withEnv(["HOME=${this.workspace}", "PATH=${this.workspace}/.k3d/bin:"+'${PATH}']) { // Make k3d write kubeconfig to WORKSPACE
+        script.withEnv(["HOME=${workspace}", "PATH=${workspace}/.k3d/bin:${path}"]) { // Make k3d write kubeconfig to WORKSPACE
             // Install k3d binary to workspace in order to avoid concurrency issues
-            sh.returnStdOut "if ! command -v k3d >/dev/null 2>&1; then " +
+
+            script.sh "if ! command -v k3d >/dev/null 2>&1; then " +
                 "curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh |" +
                 'TAG=v$(sed -n "s/^K3D_VERSION=//p" scripts/init-cluster.sh) ' +
-                "K3D_INSTALL_DIR=${this.workspace}/.k3d/bin " +
+                "K3D_INSTALL_DIR=${workspace}/.k3d/bin " +
                 'bash -s -- --no-sudo; fi'
-            sh.returnStdOut "yes | ./scripts/init-cluster.sh --cluster-name=${clusterName} --bind-localhost=false"
+            script.sh "yes | ./scripts/init-cluster.sh --cluster-name=${clusterName} --bind-localhost=false"
         }
     }
 
@@ -51,9 +50,9 @@ class K3d {
      *
      * @param clusterName The name of the cluster you want to delete
      */
-    void deleteK3d(String clusterName) {
-        withEnv(["PATH=${this.workspace}/.k3d/bin:"+'${PATH}']) {
-            sh.returnStdOut "k3d cluster delete ${clusterName}"
+    void deleteK3d() {
+        script.withEnv(["PATH=${workspace}/.k3d/bin:${path}"]) {
+            script.sh "k3d cluster delete ${clusterName}"
         }
     }
 
@@ -63,7 +62,7 @@ class K3d {
      * @param command The kubectl command you want to execute, without the leading "kubectl"
      */
     void kubectl(command) {
-        sh.returnStdOut "sudo KUBECONFIG=${this.workspace}/.kube/config kubectl ${command}"
+        script.sh "sudo KUBECONFIG=${workspace}/.kube/config kubectl ${command}"
     }
 
 }
