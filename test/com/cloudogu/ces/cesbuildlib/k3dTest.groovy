f0dd0f7b3963e1c7986834477e61d9b3613dbcd5
Robert Auer <robert.auer@cloudogu.com>
2022-03-02 10:04:18 +0100

    Check that the cluster name is more than just citest- ; #67

--- test/com/cloudogu/ces/cesbuildlib/k3dTest.groovy
+++ test/com/cloudogu/ces/cesbuildlib/k3dTest.groovy
@@ -5,6 +5,7 @@ class k3dTest extends GroovyTestCase {
         K3d sut = new K3d("script","workspace", "path")
         String testClusterName = sut.createClusterName()
         assertTrue(testClusterName.contains("citest-"))
+        assertTrue(testClusterName != "citest-")
         assertTrue(testClusterName.length() <= 32)
         String testClusterName2 = sut.createClusterName()
         assertTrue(testClusterName != testClusterName2)
