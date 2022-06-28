a6c52108888eac45a55ccdb8cf86bba016690f83
Vincent <vincent.poneleit@cloudogu.com>
2022-06-28 08:51:55 +0200

    #81 add Test

--- src/com/cloudogu/ces/cesbuildlib/Markdown.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Markdown.groovy
@@ -14,8 +14,9 @@ class Markdown implements Serializable{
     def check(){
         this.docker.image("ghcr.io/tcort/markdown-link-check:stable")
             .mountJenkinsUser()
-            .inside("--entrypoint=\"\" -v ${this.script.env.WORKSPACE}/docs:/tmp") {
-                this.script.sh 'find /tmp -name \\*.md -print0 | xargs -0 -n1 markdown-link-check -v'
+            .inside("--entrypoint=\"\" -v ${this.script.env.WORKSPACE}/docs:/docs") {
+                this.script.sh 'find /docs -name \\*.md -print0 | xargs -0 -n1 markdown-link-check -v'
             }
     }
+
 }
