6c7c5eab73fe6b077b3eb62fd8b4b7d3b8c0feb1
Vincent <vincent.poneleit@cloudogu.com>
2022-06-28 11:31:19 +0200

    #81 add Test

--- test/com/cloudogu/ces/cesbuildlib/MarkdownTest.groovy
+++ test/com/cloudogu/ces/cesbuildlib/MarkdownTest.groovy
@@ -1,15 +1,18 @@
 package com.cloudogu.ces.cesbuildlib
 
 import org.junit.Test
+import org.mockito.Mockito
 import org.mockito.invocation.InvocationOnMock
 import org.mockito.stubbing.Answer
 
+import static org.mockito.Mockito.times
+import static org.mockito.Mockito.verify
 import static org.mockito.Mockito.when
 
 
 class MarkdownTest extends GroovyTestCase {
     @Test
-    void testIfDockerIsCalledWithCorrectArgs() {
+    void testIfDockerContainerCommandIsCalledWithCorrectArgs() {
         Docker dockerMock = DockerMock.create()
         ScriptMock scriptMock = new ScriptMock(dockerMock)
         Markdown markdown = new Markdown(scriptMock)
@@ -17,13 +20,10 @@ class MarkdownTest extends GroovyTestCase {
         markdown.docker = dockerMock
 
         markdown.check()
-        when(dockerMock.image("ghcr.io/tcort/markdown-link-check:stable")).thenAnswer(new Answer<Object>() {
-            @Override
-            Object answer(InvocationOnMock invocation) throws Throwable {
-                scriptMock.sh("docker image call")
-            }
-        })
-        assert scriptMock.allActualArgs.size() == 8
-        assert scriptMock.allActualArgs[0] == "docker build call"
+
+        assert scriptMock.allActualArgs.size() == 1
+        assert scriptMock.allActualArgs[0] == "find /tmp -name \\*.md -print0 | xargs -0 -n1 markdown-link-check -v"
+
+        verify(dockerMock.image(""), times(1)).mountJenkinsUser()
     }
 }
