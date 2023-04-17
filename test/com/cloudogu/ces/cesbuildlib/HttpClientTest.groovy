ddb95baaa0ac22568d6f44297dc9755296efae96
Niklas <niklas.hinze@cloudogu.com>
2023-04-17 10:44:40 +0200

    #102 Escape single quotes in all sh arguments to prevent injection

--- test/com/cloudogu/ces/cesbuildlib/HttpClientTest.groovy
+++ test/com/cloudogu/ces/cesbuildlib/HttpClientTest.groovy
@@ -21,7 +21,7 @@ class HttpClientTest {
         assertThat(actualResponse.httpCode).isEqualTo('201')
         assertThat(actualResponse.headers['location']).isEqualTo('https://some:/url')
         assertThat(actualResponse.body).isEqualTo('')
-        assertThat(scriptMock.actualShMapArgs[0]).isEqualTo('curl -i -X GET \'http://url\'')
+        assertThat(scriptMock.actualShMapArgs[0]).isEqualTo("curl -i -X 'GET' 'http://url'")
     }
 
     @Test
@@ -43,7 +43,7 @@ class HttpClientTest {
         assertThat(actualResponse.body).isEqualTo('')
 
         assertThat(scriptMock.actualShMapArgs[0])
-            .isEqualTo('curl -i -X POST -H \'Content-Type: input\' -d \'{"title":"t","description":"d"}\' \'http://some-url\'' )
+            .isEqualTo("curl -i -X 'POST' -H 'Content-Type: input' -d '{\"title\":\"t\",\"description\":\"d\"}' 'http://some-url'" )
     }
 
     @Test
@@ -60,7 +60,7 @@ class HttpClientTest {
         assertThat(actualResponse.body).isEqualTo('')
 
         assertThat(scriptMock.actualShMapArgs[0])
-            .isEqualTo('curl -i -X PUT -H \'Content-Type: input\' -T \'/path/to/file\' \'http://some-url\'' )
+            .isEqualTo("curl -i -X 'PUT' -H 'Content-Type: input' -T '/path/to/file' 'http://some-url'")
     }
 
     @Test
@@ -82,7 +82,7 @@ class HttpClientTest {
         assertThat(actualResponse.body).isEqualTo(expectedBody1 + expectedBody2)
 
         assertThat(scriptMock.actualShMapArgs[0])
-            .isEqualTo('curl -i -X POST \'http://some-url\'' )
+            .isEqualTo("curl -i -X 'POST' 'http://some-url'")
     }
 
     @Test
@@ -97,7 +97,49 @@ class HttpClientTest {
         
         http.get('http://url')
 
-        assertThat(scriptMock.actualShMapArgs[0]).isEqualTo("curl -i -X GET -u 'user:pw' 'http://url'")
+        assertThat(scriptMock.actualShMapArgs[0]).isEqualTo("curl -i -X 'GET' -u 'user:pw' 'http://url'")
+    }
+
+    @Test
+    void "put request with single quotes"() {
+        http = new HttpClient(scriptMock, "credentialsID")
+        def expectedResponse = 'HTTP/1.1 203\n' +
+            'cache-control: no-cache\n' +
+            'content-type: output'
+        scriptMock.env.put("CURL_USER", "us'er")
+        scriptMock.env.put("CURL_PASSWORD", "p'w")
+        scriptMock.expectedDefaultShRetValue = expectedResponse
+
+        def actualResponse = http.putFile('http://so\'me-url', 'in\'put', "/path/t\'o/file")
+
+        assertThat(actualResponse.httpCode).isEqualTo('203')
+        assertThat(actualResponse.headers['content-type']).isEqualTo('output')
+        assertThat(actualResponse.body).isEqualTo('')
+
+        assertThat(scriptMock.actualShMapArgs[0])
+            .isEqualTo("curl -i -X 'PUT' -u 'us'\"'\"'er:p'\"'\"'w' -H 'Content-Type: in'\"'\"'put' -T '/path/t'\"'\"'o/file' 'http://so'\"'\"'me-url'" )
+    }
+
+    @Test
+    void "post request with single quotes"() {
+        def expectedResponse = 'HTTP/1.1 203\n' +
+            'cache-control: no-cache\n' +
+            'content-type: output'
+        scriptMock.expectedDefaultShRetValue = expectedResponse
+
+        def dataJson = JsonOutput.toJson([
+            title      : 't',
+            description: 'd\'d'
+        ])
+
+        def actualResponse = http.post('http://some-url', 'input', dataJson)
+
+        assertThat(actualResponse.httpCode).isEqualTo('203')
+        assertThat(actualResponse.headers['content-type']).isEqualTo('output')
+        assertThat(actualResponse.body).isEqualTo('')
+
+        assertThat(scriptMock.actualShMapArgs[0])
+            .isEqualTo("curl -i -X 'POST' -H 'Content-Type: input' -d '{\"title\":\"t\",\"description\":\"d'\"'\"'d\"}' 'http://some-url'" )
     }
 }
 
