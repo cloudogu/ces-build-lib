2030cff2f96c572a2a92f8c96e546bf24f1bf7bd
Robert Auer <robert.auer@cloudogu.com>
2023-08-31 10:17:04 +0200

    Fix script commands

--- src/com/cloudogu/ces/cesbuildlib/Dockerfile.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Dockerfile.groovy
@@ -2,19 +2,17 @@ package com.cloudogu.ces.cesbuildlib
 
 class Dockerfile {
     private script
-    private Sh sh
 
     Dockerfile(script) {
         this.script = script
-        this.sh = new Sh(script)
     }
 
     /**
      * Lints the Dockerfile with the latest version of hadolint
      */
     void lint(){
-        docker.image('hadolint/hadolint:latest-debian').inside(){
-            sh "hadolint Dockerfile"
+        script.docker.image('hadolint/hadolint:latest-debian').inside(){
+            script.sh "hadolint Dockerfile"
         }
     }
 }
