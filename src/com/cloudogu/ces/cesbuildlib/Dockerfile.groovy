7ce3d553bceade644d4fa9b708b3ef2855d5ef0e
Robert Auer <robert.auer@cloudogu.com>
2023-08-31 11:22:54 +0200

    Add default Dockerfile linting function; #111

--- src/com/cloudogu/ces/cesbuildlib/Dockerfile.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Dockerfile.groovy
@@ -9,10 +9,24 @@ class Dockerfile {
 
     /**
      * Lints the Dockerfile with the latest version of hadolint
+     *
+     * To configure hadelint, add a ".hadolint.yaml" file to your working directory
+     * See https://github.com/hadolint/hadolint#configure
      */
-    void lint(){
+    void lint(String dockerfile = "Dockerfile"){
         script.docker.image('hadolint/hadolint:latest-debian').inside(){
-            script.sh "hadolint Dockerfile"
+            script.sh "hadolint ${dockerfile}"
+        }
+    }
+
+    /**
+     * Lints the Dockerfile with the latest version of hadolint
+     * Only fails on errors, ignores warnings etc.
+     * Trusts registries docker.io, gcr.io and registry.cloudogu.com
+     */
+    void lintDefault(String dockerfile = "Dockerfile"){
+        script.docker.image('hadolint/hadolint:latest-debian').inside(){
+            script.sh "hadolint -t error --no-color --trusted-registry docker.io --trusted-registry gcr.io --trusted-registry registry.cloudogu.com ${dockerfile}"
         }
     }
 }
