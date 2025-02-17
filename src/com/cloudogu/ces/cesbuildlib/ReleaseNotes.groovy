6e607567fd7efb7684e8418064b4bb20eaeb59d1
Robert Auer <robert.auer@cloudogu.com>
2025-02-17 15:31:32 +0100

    Check both german and english release notes

--- src/com/cloudogu/ces/cesbuildlib/ReleaseNotes.groovy
+++ src/com/cloudogu/ces/cesbuildlib/ReleaseNotes.groovy
@@ -26,6 +26,13 @@ class ReleaseNotes implements Serializable {
     }
 
     /**
+     * @return Returns the content of english release notes file.
+     */
+    private String readReleaseNotesEN(){
+        script.readFile releaseNotesFileNameEN
+    }
+
+    /**
      * Extracts the changes for a given version out of the german release notes.
      *
      * @param releaseVersion The version to get the changes for.
@@ -36,6 +43,19 @@ class ReleaseNotes implements Serializable {
         def start = changesStartIndex(releaseNotesDE, releaseVersion)
         def end = changesEndIndex(releaseNotesDE, start)
         return escapeForJson(releaseNotesDE.substring(start, end).trim())
+    }
+
+    /**
+     * Extracts the changes for a given version out of the english release notes.
+     *
+     * @param releaseVersion The version to get the changes for.
+     * @return Returns the changes as String.
+     */
+    String changesForENVersion(String releaseVersion) {
+        def releaseNotesEN = readReleaseNotesEN()
+        def start = changesStartIndex(releaseNotesEN, releaseVersion)
+        def end = changesEndIndex(releaseNotesEN, start)
+        return escapeForJson(releaseNotesEN.substring(start, end).trim())
     }
 
     /**
@@ -53,31 +73,31 @@ class ReleaseNotes implements Serializable {
     }
 
     /**
-     * Returns the start index of changes of a specific release version in the german release notes.
+     * Returns the start index of changes of a specific release version in the release notes.
      *
      * @param releaseVersion The version to get the changes for.
      * @return Returns the index in the release notes string where the changes start.
      */
-    private static int changesStartIndex(String releaseNotesDE, String releaseVersion) {
-        def index = releaseNotesDE.indexOf("## [${releaseVersion}]")
+    private static int changesStartIndex(String releaseNotes, String releaseVersion) {
+        def index = releaseNotes.indexOf("## [${releaseVersion}]")
         if (index == -1){
             throw new IllegalArgumentException("The desired version '${releaseVersion}' could not be found in the release notes.")
         }
-        def offset = releaseNotesDE.substring(index).indexOf("\n")
+        def offset = releaseNotes.substring(index).indexOf("\n")
         return index + offset
     }
 
     /**
-     * Returns the end index of changes of a specific release version in the german release notes.
+     * Returns the end index of changes of a specific release version in the release notes.
      *
      * @param start The start index of the changes for this version.
      * @return Returns the index in the release notes string where the changes end.
      */
-    private static int changesEndIndex(String releaseNotesDE, int start) {
-        def releaseNotesAfterStartIndex = releaseNotesDE.substring(start)
+    private static int changesEndIndex(String releaseNotes, int start) {
+        def releaseNotesAfterStartIndex = releaseNotes.substring(start)
         def index = releaseNotesAfterStartIndex.indexOf("\n## [")
         if (index == -1) {
-            return releaseNotesDE.length()
+            return releaseNotes.length()
         }
         return index + start
     }
--- vars/checkReleaseNotes.groovy
+++ vars/checkReleaseNotes.groovy
@@ -4,9 +4,13 @@ def call(ReleaseNotes releaseNotes = new ReleaseNotes(this)) {
     // Checking if this is associated with a pull request
     if (env.CHANGE_TARGET) {
         echo "Checking release notes..."
-        String newChangesReleaseNotes = releaseNotes.changesForDEVersion("Unreleased")
-        if (!newChangesReleaseNotes || newChangesReleaseNotes.allWhitespace) {
-            unstable('Release Notes should contain new change entries in the `[Unreleased]` section but none were found.')
+        String newChangesReleaseNotesDE = releaseNotes.changesForDEVersion("Unreleased")
+        if (!newChangesReleaseNotesDE || newChangesReleaseNotesDE.allWhitespace) {
+            unstable('Release Notes should contain new change entries in the `[Unreleased]` section but none were found in the german version')
+        }
+        String newChangesReleaseNotesEN = releaseNotes.changesForENVersion("Unreleased")
+        if (!newChangesReleaseNotesEN || newChangesReleaseNotesEN.allWhitespace) {
+            unstable('Release Notes should contain new change entries in the `[Unreleased]` section but none were found in the english version')
         }
     }
 }
