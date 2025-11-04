c2e250f1ab9c0947bee792500e2527138236bac7
Marco Bergen <marco.bergen-extern@cloudogu.com>
2025-11-04 08:03:18 +0000

    [151] use yq instead of unsecure jq image

--- src/com/cloudogu/ces/cesbuildlib/K3d.groovy
+++ src/com/cloudogu/ces/cesbuildlib/K3d.groovy
@@ -395,12 +395,14 @@ class K3d {
  * @param interval Interval in seconds for querying the actual state of the setup e. g. 2
  */
     void setup(String tag, config = [:], Integer timout = 300, Integer interval = 5) {
-        docker.image("cloudogu/base:latest")
-            .mountJenkinsUser()
-            .inside("--volume ${this.workspace}:/workdir -w /workdir") {
-                String dogulist = this.sh.returnStdOut("cesapp list-remote")
-                script.echo dogulist
-            }
+        docker.withRegistry('https://registry.cloudogu.com/', "cesmarvin-setup") {
+            docker.image("official/base:3.22.0-4")
+                .mountJenkinsUser()
+                .inside("--volume ${this.workspace}:/workdir -w /workdir") {
+                    String dogulist = this.sh.returnStdOut("cesapp list-remote")
+                    script.echo dogulist
+                }
+        }
         assignExternalIP()
         configureEcosystemCoreValues(config)
         installAndTriggerSetup(tag, timout, interval)
