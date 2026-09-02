8c77ee970963b2a84e9572e5ceb28cb89e4efd71
Florian Hübner <florian.huebner@cloudogu.com>
2026-09-02 15:03:51 +0200

    #175 Mark build unstable on dead Markdown links

--- src/com/cloudogu/ces/cesbuildlib/Markdown.groovy
+++ src/com/cloudogu/ces/cesbuildlib/Markdown.groovy
@@ -1,6 +1,6 @@
 package com.cloudogu.ces.cesbuildlib
 
-class Markdown implements Serializable{
+class Markdown implements Serializable {
     Sh sh
     private script
     Docker docker
@@ -14,11 +14,19 @@ class Markdown implements Serializable{
     }
 
     def check(){
+        def exitCode
         this.docker.image("ghcr.io/tcort/markdown-link-check:${this.tag}")
             .mountJenkinsUser()
             .inside("--entrypoint=\"\" -v ${this.script.env.WORKSPACE}/docs:/docs") {
-                this.script.sh 'find /docs -name \\*.md -print0 | xargs -0 -n1 markdown-link-check -v'
+                exitCode = this.script.sh(returnStatus: true,
+                    script: 'find /docs -name \\*.md -print0 | xargs -0 -n1 markdown-link-check -v') as int
             }
+        // xargs exits 123 if any markdown-link-check found dead links
+        if (exitCode == 123) {
+            this.script.unstable("Found offline Markdown links!")
+        } else if (exitCode != 0) {
+            this.script.error("Markdown link check failed with exit code ${exitCode}")
+        }
     }
 
 }
